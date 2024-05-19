ca0b10da9ddfe55d792599ded1129be6e3a3d03a
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-05-19 06:20:44 +0330

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>authentication & authorization</title>
+        <meta name='description' content='learnin auth' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
